d1604854b73cceb6b379dd5d063a6ff1d11e841f
rogergcc <rogergcc@outlook.com>
2016-05-18 02:15:07 -0500

    Reportes lab01

--- hr_consultas.sql
+++ hr_consultas.sql
@@ -219,59 +219,136 @@ AS 'Empleados y sus Salarios'
 FROM employees
 ORDER BY salary DESC;
 
---11
+--4.5-9
+declare @fechaActual date;
+set @fechaActual = cast(getdate() As Date);
+SELECT last_name, DATEDIFF(WEEK, hire_date, @fechaActual) AS tenencia
+FROM employees
+WHERE department_id = 90
+ORDER BY tenencia DESC
 
-Select Convert(datetime ,commission_pct) 
+--4.6. Actividad N° 06 – Funciones de Conversión
+--4.6-1
+
+select CONCAT(last_name , ' gana $ ' , salary, ' pero quisiera $ ', salary*3 ) as 'Sueldos Soñados'
+from employees
+--4.6-2
+SELECT last_name, hire_date,
+	DATEADD([day], ((DATEDIFF([day], '19000101', 
+	cast(dateadd(MONTH, 6, hire_date) AS Date)) 
+	/ 7) * 7) + 7, '19000101')
+from employees
+--4.6-3
+SELECT last_name, hire_date,
+       CONVERT(VARCHAR, DATEPART(DAY, hire_date)) as Dia
+FROM     employees 
+ORDER BY CONVERT(VARCHAR, DATEPART(DAY, hire_date)); 
+
+--4.6-4
 SELECT last_name,
-       NVL(Convert(datetime ,commission_pct) , 'No Commission') COMM
+       ISNULL(Convert(varchar ,commission_pct) , 'Sin Comisión') Comisión
 FROM   employees;
 
---4 soluciones
-SELECT ROUND(MAX(salary),0) "Maximum",
-       ROUND(MIN(salary),0) "Minimum",
-       ROUND(SUM(salary),0) "Sum",
-       ROUND(AVG(salary),0) "Average"
+--4.6-5
+SELECT job_id as Puesto, CASE job_id
+               WHEN 'ST_CLERK' THEN 'E'
+               WHEN 'SA_REP'   THEN 'D'
+               WHEN 'IT_PROG'  THEN 'C'
+               WHEN 'ST_MAN'   THEN 'B'
+               WHEN 'AD_PRES'  THEN 'A'
+               ELSE '0'  END as Grado
+FROM employees; 
+
+
+--4.7. Actividad N° 07 – Funciones de Agrupación
+
+--4.7-1 Máximo, Mínimo, Suma y Promedio
+SELECT 
+	   CONVERT(int,ROUND(MAX(salary),0)) "Máximo",
+	   CONVERT(int,ROUND(MIN(salary),0)) "Mínimo",
+	   CONVERT(int,ROUND(SUM(salary),0)) "Suma",
+	   CONVERT(int,ROUND(AVG(salary),0)) "Promedio"
 FROM   employees;
---
-SELECT job_id, ROUND(MAX(salary),0) "Maximum",
-               ROUND(MIN(salary),0) "Minimum",
-               ROUND(SUM(salary),0) "Sum",
-               ROUND(AVG(salary),0) "Average"
-FROM   employees       
+--4.7-2
+SELECT 
+	   CONVERT(int,ROUND(MAX(salary),0)) "Máximo",
+	   CONVERT(int,ROUND(MIN(salary),0)) "Mínimo",
+	   CONVERT(int,ROUND(SUM(salary),0)) "Suma",
+	   CONVERT(int,ROUND(AVG(salary),0)) "Promedio"
+FROM   employees   
 GROUP BY job_id;
---
+--4.7-3
 SELECT job_id, COUNT(*)
 FROM   employees 
 GROUP BY job_id;
 -----
+--4.7-4
+SELECT COUNT(DISTINCT manager_id) "N° de Administradores"
+FROM   employees;
+--4.7-5
+SELECT   MAX(salary) - MIN(salary) Diferencia
+FROM     employees; 
+--4.7-6
+SELECT   manager_id 'N° de Administradores' , MIN(salary) 'Salario'
+FROM     employees
+WHERE    manager_id IS NOT NULL
+GROUP BY manager_id
+HAVING   MIN(salary) > 6000 
+ORDER BY MIN(salary) DESC;
+--4.7-7
+/*CONVERT(VARCHAR, DATEPART(YEAR, GETDATE()))
+TO_CHAR(SYSDATE, 'YYYY')*/
+SELECT  COUNT(*) total,
+        SUM(
+		case CONVERT(VARCHAR, YEAR(hire_date)) when 1995 then 1 else 0 end ) "1995",
+		SUM(
+		case CONVERT(VARCHAR, YEAR(hire_date)) when 1996 then 1 else 0 end) "1996",
+		SUM(
+		case CONVERT(VARCHAR, YEAR(hire_date)) when 1997 then 1 else 0 end ) "1997",
+		SUM(
+		case CONVERT(VARCHAR, YEAR(hire_date)) when 1998 then 1 else 0 end ) "1998"
+FROM    employees;
+--4.7-8
+SELECT   job_id Puesto,
+         SUM(case department_id when 20 then  salary end ) "Dept 20",
+		 SUM(case department_id when 50 then  salary end ) "Dept 50",
+		 SUM(case department_id when 80 then  salary end ) "Dept 80",
+		 SUM(case department_id when 90 then  salary end ) "Dept 90",
+         SUM(salary) "Total"
+FROM     employees
+GROUP BY job_id;
+
+--4.8. Actividad N° 08 – Enlaces
+--4.8-1
 SELECT l.location_id, l.street_address, l.city, l.state_province, c.country_name
 FROM   locations l
 join countries c on l.country_id = c.country_id
---
+--4.8-2
 SELECT e.last_name, e.department_id, d.department_name
 FROM   employees e
 join departments d on e.department_id = d.department_id
 
---
+--4.8-3
 SELECT e.last_name, e.job_id, e.department_id, d.department_name
 FROM   employees e, departments d , locations l
 WHERE  e.department_id = d.department_id
 AND    d.location_id = l.location_id
 AND    LOWER(l.city) = 'toronto';
 
---
+--4.8-4
 SELECT w.last_name "Employee", w.employee_id "EMP#", 
        m.last_name "Manager", m.employee_id  "Mgr#"
 FROM   employees w, employees m
 WHERE  w.manager_id = m.employee_id; 
 
---
+--4.8-5
 SELECT w.last_name "Employee", w.employee_id "EMP#", 
        m.last_name "Manager", m.employee_id  "Mgr#"
-FROM   employees w, employees m
-WHERE  w.manager_id  = m.employee_id ;
+FROM   employees w 
+LEFT   OUTER JOIN employees m
+ON     (w.manager_id = m.employee_id); 
 
---
+--4.8-6
 SELECT e.department_id department, e.last_name employee,
        c.last_name colleague
 FROM   employees e, employees c
@@ -279,13 +356,18 @@ WHERE  e.department_id = c.department_id
 AND    e.employee_id <> c.employee_id 
 ORDER BY e.department_id, e.last_name, c.last_name; 
 
+--4.8-7
 SELECT e.last_name, e.hire_date
 FROM   employees e , employees davies
 WHERE  davies.last_name = 'Davies'
 AND    davies.hire_date < e.hire_date;
+--4.8-8
+SELECT w.last_name, w.hire_date, m.last_name, m.hire_date
+FROM   employees w JOIN employees m
+ON    (w.manager_id = m.employee_id) 
+WHERE    w.hire_date <  m.hire_date; 
+--4.9. Actividad N° 09 – Subconsultas
 
---4.9 subconsultas
-go
 /*create procedure subConsultaAp
 (@apellido varchar(15))
 as
@@ -297,29 +379,30 @@ WHERE  department_id = (SELECT department_id
                         WHERE  last_name = @apellido)
 AND    last_name <> @apellido; 
 end*/
+--4.9-1
 exec subConsultaAp 'Zlotkey'
---
+--4.9-2
 SELECT employee_id, last_name, salary
 FROM   employees
 WHERE  salary > (SELECT AVG(salary)
                  FROM   employees)
 ORDER BY salary;
---
+--4.9-3
 SELECT employee_id, last_name
 FROM   employees
 WHERE  department_id IN (SELECT department_id
                          FROM   employees
                          WHERE  last_name like '%u%');
---
+--4.9--4
 SELECT last_name, department_id, job_id
 FROM   employees
 WHERE  department_id IN (SELECT department_id
                          FROM   departments
                          WHERE  location_id = 1700);
---
+--4.9-5
 
 exec ingreseLocalizacion 1700
-
+--4.9-6
 SELECT last_name, salary
 FROM   employees
 WHERE  manager_id in (SELECT employee_id
@@ -327,7 +410,7 @@ WHERE  manager_id in (SELECT employee_id
 					 
                      WHERE  last_name = 'King'
 					 );
-
+--4.9-7
 SELECT department_id, last_name, job_id
 FROM   employees
 WHERE  department_id IN (SELECT department_id
